11374b69a5888c2024a9fb386c2ab366a374697d
Jason Jacobson <jason.jacobson@target.com>
2019-11-26 14:28:52 -0600

    worker for movie

--- src/App.js
+++ src/App.js
@@ -24,7 +24,20 @@ class App extends Component {
           Display
         </div>
         <div className="mmr-main__bottom">
-          Bottom
+          <div className="mmr-bottom">
+            <div className="mmr-movie">
+              <div class="spx-media">
+                <div className="spx-media__element">
+                  <img src="http://image.tmdb.org/t/p/original/fapXd3v9qTcNBTm39ZC4KUVQDNf.jpg" alt="Fight Club" />
+                </div>
+                <div className="spx-media__body">
+                  <h2>Angel Has Fallen</h2>
+                  <p>After the events in the previous film, Secret Service agent Mike Banning finds himself framed for an assassination attempt on the President. Pursued by his own agency and the FBI, Banning races to clear his name and uncover the real terrorist threat which has set its sights on Air Force One.</p>
+                  <p>Vote Avg. 5.8</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
